660f8eecc3f20d38afd87a5debac1a12ed9d6b5f
Brian P O'Rourke <bpo@somnambulance.net>
2011-01-13 15:33:37 -0800

    NodeNailer vendor cache

---
+++ vendor/imagemagick@0.1.1/package.json.js
@@ -0,0 +1,78 @@
+module.exports = {
+  "name": "imagemagick",
+  "description": "A wrapper around the imagemagick cli",
+  "version": "0.1.1",
+  "author": {
+    "name": "Rasmus Andersson"
+  },
+  "licenses": [
+    {
+      "type": "MIT"
+    }
+  ],
+  "repository": {
+    "type": "git",
+    "url": "http://github.com/rsms/node-imagemagick.git"
+  },
+  "engine": [
+    "node >=0.1.90"
+  ],
+  "_id": "imagemagick@0.1.1",
+  "engines": {
+    "node": "*"
+  },
+  "_engineSupported": true,
+  "_npmVersion": "0.2.13-1",
+  "_nodeVersion": "v0.2.5",
+  "directories": {},
+  "_resolvedDeps": [],
+  "modules": {
+    "index.js": "imagemagick"
+  },
+  "_npmConfig": {
+    "root": "/Users/bpo/Projects/NodeNailer/node_modules",
+    "binroot": "/Users/bpo/Projects/NodeNailer/node_modules/.bin",
+    "manroot": null,
+    "must-install": false,
+    "showlevel": 1,
+    "registry": "http://registry.npmjs.org/",
+    "argv": {
+      "remain": [],
+      "cooked": [
+        "bundle"
+      ],
+      "original": [
+        "bundle"
+      ]
+    },
+    "auto-activate": "always",
+    "update-dependents": true,
+    "auto-deactivate": true,
+    "tar": "tar",
+    "gzipbin": "gzip",
+    "tag": "latest",
+    "proxy": null,
+    "globalconfig": "/usr/local/etc/npmrc",
+    "userconfig": "/Users/bpo/.npmrc",
+    "dev": false,
+    "loglevel": "info",
+    "tmproot": "/var/folders/Ix/IxJ6twmSGYyhoBgkOK7zO++++TI/-Tmp-/",
+    "logfd": 2,
+    "outfd": 1,
+    "color": true,
+    "recursive": false,
+    "force": false,
+    "listopts": "",
+    "browser": "open",
+    "description": true
+  },
+  "_npmPaths": {
+    "root": "/Users/bpo/Projects/NodeNailer/node_modules",
+    "dir": "/Users/bpo/Projects/NodeNailer/node_modules/.npm",
+    "cache": "/Users/bpo/Projects/NodeNailer/node_modules/.npm/.cache",
+    "tmp": "/var/folders/Ix/IxJ6twmSGYyhoBgkOK7zO++++TI/-Tmp-/npm-1294961598079",
+    "package": "/Users/bpo/Projects/NodeNailer/node_modules/.npm/imagemagick/0.1.1/package",
+    "modules": "/Users/bpo/Projects/NodeNailer/node_modules/imagemagick@0.1.1",
+    "dependencies": "/Users/bpo/Projects/NodeNailer/node_modules/.npm/imagemagick/0.1.1/node_modules"
+  }
+}
